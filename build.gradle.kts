4567c94c36c6e10969cb9bc878e49fafe0a774ba
hadyatullo <hadyatullo@abad.tj>
2020-08-15 19:20:14 +0500

    removed unused libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,13 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.hibernate:hibernate-envers:5.4.14.Final")
-	runtimeOnly("org.postgresql:postgresql")
+//	implementation("org.hibernate:hibernate-envers:5.4.14.Final")
+//	runtimeOnly("org.postgresql:postgresql")
 
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
