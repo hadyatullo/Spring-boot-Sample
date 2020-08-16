9608f31e55feff5183cb954d3ffbf29e7bf40d89
hadyatullo <hadyatullo@abad.tj>
2020-08-16 11:57:05 +0500

    divided into layers

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //	implementation("org.hibernate:hibernate-envers:5.4.14.Final")
 //	runtimeOnly("org.postgresql:postgresql")
-
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
